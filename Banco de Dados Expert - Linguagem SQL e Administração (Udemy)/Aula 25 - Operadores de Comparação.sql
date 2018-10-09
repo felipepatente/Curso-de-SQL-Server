3c2333bef2792d8c0446c6b580f70df1acb03c59
felipepatente <felipepatente@yahoo.com.br>
2018-10-08 21:39:01 -0300

    Aula 25 - Operadores de Comparação

--- Banco de Dados Expert - Linguagem SQL e Administração (Udemy)/Aula 25 - Operadores de Comparação.sql
+++ Banco de Dados Expert - Linguagem SQL e Administração (Udemy)/Aula 25 - Operadores de Comparação.sql
@@ -1,42 +1,41 @@
---Usando operador = 
-	SELECT * FROM   cidades 
-	WHERE  uf ='SP'
+--Usando operador
+SELECT * FROM cidades
+WHERE uf = 'SP'
 
---Outro exemplo operador =
-	SELECT * FROM   cidades 
-	WHERE nome_mun = 'Dourado'      
-	AND uf = 'SP' 
+--Outro Exemplo operador = 
+SELECT * FROM cidades
+WHERE nome_mun = 'Dourado'
 
---Usando operador > 
-	SELECT * FROM   cidades 
-	WHERE  populacao > 100000 
+--Usando operador >
+SELECT * FROM cidades
+WHERE populacao > 100000
 
---Usando operador > 
-	SELECT * FROM   cidades 
-	WHERE  populacao > 1000000
+--Usando operador >
+SELECT * FROM cidades
+WHERE populacao > 100000
 
---Usando operador < 
-	SELECT * FROM   cidades 
-	WHERE  populacao < 10000 
---Usando operador < 
-	SELECT * FROM   cidades 
-	WHERE  populacao < 50000
+--Usando operador <
+SELECT * FROM cidades
+WHERE populacao < 100000
 
---Usando operador <= 
-	SELECT * FROM   cidades 
-	WHERE  populacao <= 10000 
---Usando operador <= 
-	SELECT * FROM   cidades 
-	WHERE  populacao <= 50000
+--Usando operador <
+SELECT * FROM cidades
+WHERE populacao < 50000
 
---Usando operador <> 
-	SELECT * FROM   cidades 
-	WHERE  uf <> 'SP'        
-	AND uf <> 'SC'
+--Usando operador <=
+SELECT * FROM cidades
+WHERE populacao <= 100000
 
---Combinando operadores 
-	SELECT * FROM   cidades 
-	WHERE  populacao <= 100000        
-		AND populacao >= 50000        
-		AND uf = 'SP'        
-		AND nome_mun <> 'Vinhedo'
+--Usando operador <=
+SELECT * FROM cidades
+WHERE populacao <= 50000
+
+--Usando operador <>
+SELECT * FROM cidades
+WHERE uf <> 'SP' AND uf <> 'SC'
+
+--Combinando Operadores
+SELECT * FROM cidades
+WHERE populacao <= 10000
+	AND populacao >= 5000
+	AND nome_mun = 'Vinhedo'
